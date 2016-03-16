a0aab606150660ef1ca1eeeae5f48552210e869a
Liam Dickson <liamsongdickson@gmail.com>
2016-03-16 09:12:51 -0400

    started conversion to python, added README

--- classes.py
+++
@@ -1,9 +0,0 @@
-import re
-
-class Production:
-    def __init__(self, string):
-        p = re.compile("->[0-9]+")
-        m = p.search(string)
-        print m.groups()
-
-Production("S ->0 A B")
--- errorparser.py
+++ errorparser.py
@@ -1,3 +1,33 @@
-class Grammar:
-    def import_production(self, prod):
-        self.prod
+import sys
+from errorclasses import *
+
+"""Takes a grammar and an input string and returns a tuple of the closest string
+in the grammar for that input string and the distance of the input string to the
+grammar (number of errors).
+"""
+def error_correcting_parser(g, input_string):
+    n = len(input_string)
+    lookup = Lookup(g.productions, n)
+
+def main(argv):
+    try:
+        if (len(argv) == 0):
+            grammar_file = open('grammar.txt')
+            input_string = "tactagcaatacgcttgcgttcggtggttaagtatgtataatgcgcgggcttgtcgt"
+        elif (len(argv) == 1):
+            grammar_file = open('grammar.txt')
+            input_string = argv[0]
+        elif (len(argv) == 2):
+            input_string = argv[0]
+            grammar_file = open(argv[1])
+    except:
+        print "Must use format: python errorparser.py <input_string>\
+        <grammar_file>"
+
+    g = Grammar()
+    for line in grammar_file:
+        g.add_production(line)
+    error_correcting_parser(g, input_string)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
