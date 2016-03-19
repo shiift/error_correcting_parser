620b61ce8cf77c8cf9f2263cb532da0be7c6b2c3
Liam Dickson <liam.s.dickson@gmail.com>
2016-03-19 17:16:32 -0400

    added comments, needs some work on the solving of I'

--- errorparser.py
+++ errorparser.py
@@ -4,6 +4,8 @@ import argparse
 
 class BreakIt(Exception): pass
 
+"""Production conatins a left hand side, right hand side and number of errors
+for the production."""
 class Production:
     regex = r'->[0-9]+'
     def __init__(self, arg0, arg1=None, arg2=None):
@@ -28,7 +30,10 @@ class Production:
     def __repr__(self):
         return self.lhs + " ->" + str(self.errors) + " " + self.rhs
 
+"""Grammar conatins a list of productions, a list of terminals, a list of
+non-terminals, and the top level symbol character ('S' by default)"""
 class Grammar:
+    S = 'S'
     productions = []
     terminals = []
     nonterminals = []
@@ -87,6 +92,17 @@ class Node:
         self.p = p
         self.left = None
         self.right = None
+    def print_node(self, node, i, s):
+        if node == None:
+            return ""
+        for j in range(i):
+            s += " "
+        sn = s + str(node.p) + "\n"
+        sl = self.print_node(node.left, i+1, s)
+        sr = self.print_node(node.right, i+1, s)
+        return sn + sl + sr
+    def __repr__(self):
+        return self.print_node(self, 0, "")
 
 """Takes a grammar and an input string and returns a tuple of the closest string
 in the grammar for that input string and the distance of the input string to the
@@ -117,12 +133,16 @@ def error_correcting_parser(g, input_string):
                             M.insert(i, i+s, (A,l))
                             X.insert(A, (i,i+s,l))
     best = None
-    for (j, k, l) in X.get('S', 1):
+    for (j, k, l) in X.get(g.S, 1):
         if (k == n + 1) and (not best or l < best):
             best = l
-    tree = parse_tree(M, 'S', 1, n+1, best, input_string, g.nonterminals)
+    tree = parse_tree(M, g.S, 1, n+1, best, input_string, g.nonterminals)
     return (best, tree)
 
+"""Takes a Matrix, a symbol, a start location, an end location, the best error
+distance for the string, and a list of nonterminals and returns a parse tree
+for the individual characters in the string. This can be used to find I'.
+"""
 def parse_tree(M, D, i, j, l, a, NT):
     if i == j - 1:
         for (Dc, lc) in M.get(i, j):
@@ -149,24 +169,33 @@ def parse_tree(M, D, i, j, l, a, NT):
     r.left = T1
     r.right = T2
     return r
-
-def flatten_tree(t, g, s):
-    if t == None:
+"""This takes a parse tree, a list of terminals and an empty string, and returns
+a string for the closest string in the list of terminals for the tree.
+"""
+def flatten_tree(tree, ts, s):
+    if tree == None:
         return ""
-    if t.left == None and t.right == None:
-        return find_correction(t.p, g)
-    c = flatten_tree(t.left, g, s)
-    s += c + flatten_tree(t.right, g, s)
+    if tree.left == None and tree.right == None:
+        return find_correction(tree.p, ts)
+    c = flatten_tree(tree.left, ts, s)
+    s += c + flatten_tree(tree.right, ts, s)
     return s
 
+"""Takes a production and a list of terminals and returns the right hand side of
+the production if has 0 errors, otherwise returns the rhs of a terminal that
+matches the lhs of the production with 0 errors.
+"""
 def find_correction(p, ts):
     if p.errors == 0:
         return p.rhs
     for t in ts:
         if t.lhs == p.lhs and t.errors == 0:
             return t.rhs
-    raise ValueError('Could not find an in-language symbol to character map')
+    raise ValueError('Could not find an in-language symbol to map: ' + str(p))
 
+"""Takes a grammar and an input string and runs the parser. This function prints
+out the Input string, the closest string in the grammar (I') and the number of
+errors between them"""
 def run_parser(g, input_string):
     e, tree = error_correcting_parser(g, input_string)
     print "I :" + input_string
